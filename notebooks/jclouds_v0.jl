d5e4e2b251ff4d26f7bfede534770af2f676be09
jahernando <hernando.joseangel@gmail.com>
2022-09-22 18:31:06 +0200

    towards code in pkg

--- notebooks/jclouds_v0.jl
+++ notebooks/jclouds_v0.jl
@@ -14,7 +14,6 @@ using HDF5
 using DataFrames
 using StatsBase
 using Plots
-using StatsBase
 using LinearAlgebra
 using Statistics
 using PlutoUI
@@ -27,6 +26,9 @@ end
 
 # ╔═╡ 1751666d-d26f-4d8e-b5d6-b36854dd7462
 #Pkg.add("Distributions")
+
+# ╔═╡ a57cdb41-c388-4976-bec8-ec0650fb139c
+import jclouds as jc
 
 # ╔═╡ cdc50171-b288-40b6-9d0d-9511901218e0
 md"""
@@ -78,49 +80,29 @@ function _random()
 end
 end
 
-# ╔═╡ e4e0ac6b-ad55-4a96-81e7-c5ec8df53be6
+# ╔═╡ f5fb4efe-3d08-4d75-8855-670d0fe92f25
 begin
-nn = 10
-zz = 0:nn-1
-zz = vcat(zz, reverse(zz))
-zz = ones(nn)' .* zz
-println(Base.size(zz))
-xx = 0.5 .+ 0:2*nn
-yy = 0.5 .+ 0:nn
-xm = ones(nn)' .* xx
-ym = yy'       .* ones(2*nn)
-xbins = 0:1:2*nn
-ybins = 0:1:nn
+ximg = jc.cc_braid()
 end
 
 # ╔═╡ 6faf5d14-f3f2-4549-bc1b-7d46771eb1da
-zz
-
-# ╔═╡ b7fb1cb1-941f-4bfa-ad96-7addfd1e60c0
-img = fit(Histogram, (vec(xm), vec(ym)), weights(vec(zz)), (xbins, ybins))
-
-# ╔═╡ 5cc3372f-f9df-4063-9e4c-dfa4e43f316c
-plot(img)
+begin
+xm, ym, zz   = vec(ximg.coors[1]), vec(ximg.coors[2]), vec(ximg.contents)
+xbins, ybins = ximg.edges[1], ximg.edges[2]	
+h1 = fit(Histogram, (xm, ym), weights(zz), (xbins, ybins))
+plot(h1)
+end
 
 # ╔═╡ 8f49762d-9e98-4fed-b55c-17d9425d0fff
 histogram2d(vec(xm), vec(ym), weights = vec(zz), nbins = (xbins, ybins))
 
-# ╔═╡ 602dc3e2-bd48-4322-8687-42a03e1d0dd7
-function histo_to_data(h::Histogram)
-	nx, ny  = Base.size(h.weights)
-	centers = [(edge[2:end] + edge[1:end-1])./2 for edge in h.edges]
-	xm = ones(ny)'   .* centers[1]
-	ym = centers[2]' .* ones(nx)
-	zz = deepcopy(h.weights)
-	return (x = vec(xm), y = vec(ym), weights = vec(zz))
-end
-
-
-# ╔═╡ b06f35cd-368a-4eec-b21d-68f70afe59fa
-data = histo_to_data(img)
-
 # ╔═╡ 62867032-c903-4b5b-b6db-51c02ddc6e6d
-histogram2d(data.x, data.y, weights = data.weights, bins = (xbins, ybins))
+begin
+ximg2 = jc.histo_to_img(h1)
+x2, y2, z2 = vec(ximg2.coors[1]), vec(ximg2.coors[2]), vec(ximg2.contents) 
+histogram2d(x2, y2, weights = z2, bins = ximg2.edges)
+#histogram2d(ximg2.coors..., weights = ximg2.contents, bins = ximg2.edges)
+end
 
 # ╔═╡ bc0a9ad4-62f4-4ef9-909a-4a2f68584b6f
 md"""
@@ -139,240 +121,13 @@ md"""
 
 """
 
-# ╔═╡ 6890e500-b267-4005-859f-405ac0ef895a
-begin
-	
-struct Moves2D
-	moves
-	imove0
-	dmoves
-	omoves
-end
-
-function _moves2d()
-	moves  = [[i, j] for i in -1:1:1 for j in -1:1:1]
-	imove0 = [i for (i, move) in enumerate(moves) if move == [0, 0]][1]
-	dmoves = Dict(1:9 .=> moves)
-	omoves = Dict()
-	imoves = deepcopy(moves)
-	imoves = filter(move -> move != imoves[imove0], moves)
-	for i in 1:9
-		vals = 
-		omoves[moves[i]] = [imove for imove in imoves 
-			if dot(imove, moves[i]) == 0]	
-	end
-	str_moves = Moves2D(moves, imove0, dmoves, omoves)
-	#str_moves.moves  = moves
-	#str_moves.imove0 = imove0
-	#str_moves.dmoces = dmoces
-	#str_moves.omoces = omoves
-end
-	
-end # begin
-
-# ╔═╡ 26bd9352-81d8-49de-a933-d62f22c462fa
-mov2d = _moves2d()
-
-# ╔═╡ cbf54f5a-1bdc-4473-b825-7d2f75211fc0
-begin
-	
-function _deltas(coors, energy, edges, steps, m)
-	
-	xstep, ystep   = steps
-	xx, yy         = coors
-	xedges, yedges = edges
-	
-	his      = [fit(Histogram, (xx .- mx * xstep, yy .- my * ystep), weights(energy), (xedges, yedges)) for (mx, my) in m.moves]
-	contents = deepcopy(his[m.imove0].weights)
-	deltas   = [h.weights .- contents for h in his]
-	
-	dsteps   = [norm([xstep, ystep] .* move) for move in m.moves]
-	dsteps[m.imove0] = 1.
-	deltas  = [delta ./dstep for (delta, dstep) in zip(deltas, dsteps)]
-	
-	return his, deltas
-end
-
-function _grad(deltas, m)
-	nx, ny = Base.size(deltas[m.imove0])
-	d0     = deltas[m.imove0]
-	grad   = deepcopy(d0)
-	igrad  = m.imove0 .* ones(Int, nx, ny)
-	for (i, di) in enumerate(deltas)
-		imask         = di .> grad
-		grad[imask]  .= di[imask]
-		igrad[imask] .= i
-	end
-	return grad, igrad
-end
-
-
-function _curvatures(deltas, m)
-
-	ddeltas = Dict()
-	for i in 1:9
-		ddeltas[m.moves[i]] = deltas[i]
-	end
-	
-	curvs = Dict()
-	for imove in m.moves
-		curvs[imove] = reduce(.+, [ddeltas[move] for move in m.omoves[imove]])
-	end
-
-	#dels = [reduce(.+, [ddeltas[i] for move in m.omoves[imove]]) for imove in m.moves] 
-
-	return curvs
-end
-	
-function _maxmin_curvatures(curves, m)
-	nx, ny = Base.size(curves[m.moves[m.imove0]])
-	curmin  =  1e6 .* ones(nx, ny)
-	icurmin = m.imove0 .* ones(Int, nx, ny) 
-	curmax  = -1e6 .* ones(nx, ny)
-	icurmax = m.imove0 .* ones(Int, nx, ny) 
-	for (i, move) in enumerate(m.moves)
-		dd = curves[move]
-		mask1 = dd .> curmax
-		curmax[mask1] .= dd[mask1]
-		icurmax[mask1] .= i
-		mask2 = dd .< curmin
-		curmin[mask2] .= dd[mask2]
-		icurmin[mask2] .= i 
-	end
-	return curmax, icurmax, curmin, icurmin
-end
-
-#--- Modes
-
-function _node(i, j, igrad, m)
-	index = CartesianIndex((i, j)...)
-	imove = igrad[index]
-	if (imove == m.imove0)
-		return index
-	else
-		index = [i, j] + m.moves[imove]
-		return _node(index[1], index[2], igrad, m)
-	end
-end
-
-function _nodes(contents, igrad, m)
-
-	cells = findall(x -> x.>0, contents)
-	nodes = [_node(cell[1], cell[2], igrad, m) for cell in cells]
-	unodes = unique(nodes)
-	inodes = Dict()
-	for (i, node) in enumerate(unodes)
-		inodes[node] = i
-	end
-	
-	xnodes = Int.(0 .* deepcopy(contents))
-	for (k, cell) in enumerate(cells)
-		xnodes[cell] = inodes[nodes[k]]
-	end
-	
-	return xnodes
-end
-
-function _neighbour_node(coors, nodes, edges, steps, m)
-	
-	xstep, ystep   = steps
-	xx, yy         = coors
-	xedges, yedges = edges
-	
-	his      = [fit(Histogram, (vec(xx) .- mx * xstep, vec(yy) .- my * ystep), weights(vec(nodes)), (xedges, yedges)) for (mx, my) in m.moves]
-	contents = deepcopy(his[m.imove0].weights)
-	mask     = contents .> 0
-	borders  = [(h.weights .>0) .* (h.weights .!= contents) for h in his] 
-	nborders = reduce(.+, borders) .* mask
-		
-	return nborders, [h.weights for h in his]
-end
-
-#--- Public
-	
-function mesh(edges)
-	nx, ny  = length(edges[1])-1, length(edges[2])-1
-	centers = [(edge[2:end] + edge[1:end-1])./2 for edge in edges]
-	xm = ones(ny)'   .* centers[1]
-	ym = centers[2]' .* ones(nx)
-	return xm, ym
-end
-	
-	
-function quiver(idir, m, steps)
-	xstep, ystep = steps
-	uus = [m.dmoves[k] for k in vec(idir)]
-	us = [xi * 0.8 * xstep for (xi, yi) in uus]
-	vs = [yi * 0.8 * ystep for (xi, yi) in uus]	
-	return us, vs
-end
-	
-function kkclouds(coors, energy, steps)
-	
-	ndim  = length(coors)
-	nsize = length(coors[1])
-
-	# assert dimensions
-	for i in 2:ndim
-		@assert(length(coors[2]) == nsize)
-	end
-	@assert(length(energy) == nsize)
-	@assert(length(steps)  == ndim)
-
-	# define the extended edges 
-	edges = [minimum(x)-1.5*step:step:maximum(x)+1.5*step 
-		for (x, step) in zip(coors, steps)]
-
-	# alias
-	xx, yy         = coors
-	xedges, yedges = edges
-	
-	# main histogram
-	histo    = fit(Histogram, (xx, yy), weights(energy), (xedges, yedges))
-	contents = deepcopy(histo.weights)
-
-	# deltas
-	m = _moves2d()
-	his, deltas = _deltas(coors, energy, edges, steps, m)
-
-	# gradient
-	grad, igrad = _grad(deltas, m)
-
-	# curvatures
-	curves = _curvatures(deltas, m)
-	lap    = curves[m.moves[m.imove0]]
-	
-	# maximum and monimum curvatures
-	curmax, icurmax, curmin, icurmin = _maxmin_curvatures(curves, m)
-
-	xs, ys = mesh(edges)
-	
-	# nodes
-	xnodes = _nodes(contents, igrad, m)
-	xborders, xneigh = _neighbour_node((xs, ys), xnodes, edges, steps, m)
-	#nborders, neighbour_node = _neighbour_node(coors, xnodes, edges, steps, m)
-	
-	# output
-	return (edges = (xedges, yedges),
-		    x = xs, y = ys, contents = contents,
-			grad = grad, igrad = igrad,
-			lap = lap, curves = curves,
-			curmax = curmax, icurmax = icurmax,
-		    curmin = curmin, icurmin = icurmin,
-			nodes  = xnodes,
-			nborders = xborders, neighbour_node = xneigh)
-
-end
-	
-end #begin
-
 # ╔═╡ 441a1b38-7ff8-456c-8511-98f57828b26b
 begin
 	steps_   = [1., 1.]
-	coors_   = (data.x, data.y)
-	weights_ = data.weights
-	mm       = _moves2d()
-	cl       = kkclouds(coors_, weights_, steps_)
+	coors_   = (vec(ximg.coors[1]), vec(ximg.coors[2]))
+	weights_ = vec(ximg.contents)
+	mm       = jc.cc_moves2d()
+	cl       = jc.cc_clouds(coors_, weights_, steps_)
 end;
 
 # ╔═╡ 7e1adeb7-2714-4205-b895-62cbe0477008
@@ -384,29 +139,32 @@ md"""
 """
 
 # ╔═╡ bd1711b7-b6c3-4a97-a54e-a7c1e268ef92
-cl.edges
+begin
+xs = vec(cl.coors[1])
+ys = vec(cl.coors[2])
+end;
 
 # ╔═╡ b774e74d-0843-4f5c-98b9-9103bd0a5657
-histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.contents), 
-		bins = cl.edges, alpha = 0.8, title = "energy")
+histogram2d(xs, ys, weights = vec(cl.contents), 
+		bins = cl.edges, alpha = 0.8, title = "contents")
 	
 
 # ╔═╡ a684c917-3750-4d80-a86e-7cc3fd7b9d02
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.grad), 
+	histogram2d(xs, ys, weights = vec(cl.grad), 
 		bins = cl.edges, alpha = 0.8, title = "gradient")
-	quiver!(vec(cl.x), vec(cl.y), quiver = quiver(cl.igrad, mm, steps_))
+	quiver!(xs, ys, quiver = jc.cc_quiver(cl.igrad, mm, steps_))
 end
 
 # ╔═╡ d8d579a5-aa12-4128-b2ff-252d165cd2a6
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.igrad), 
+	histogram2d(xs, ys, weights = vec(cl.igrad), 
 		bins = cl.edges, alpha = 0.8, title = "gradient direction")
 	#quiver!(vec(cl.x), vec(cl.y), quiver = quiver(cl.igrad, mm, steps_))
 end
 
 # ╔═╡ 1cf114b7-3f8e-4f4c-bdcd-81e0b6ddee74
-histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.lap), 
+histogram2d(xs, ys, weights = vec(cl.lap), 
 		bins = cl.edges, alpha = 1., title = "laplacian")
 
 # ╔═╡ fbdb0b59-f26f-42e5-a83a-a24962b09876
@@ -420,54 +178,54 @@ end
 
 # ╔═╡ 55c5b8d3-9ac5-4ed1-8ec5-95cf74f81e6e
 begin
-chis2 = [histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.curves[m]), 
+chis2 = [histogram2d(xs, ys, weights = vec(cl.curves[m]), 
 		bins = cl.edges, title = m) for m in mm.moves]
 plot(chis2..., layout = (3, 3))
 end
 
 # ╔═╡ c7dbc705-c905-4fc8-b1a5-616345d029b8
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.curmax), 
+	histogram2d(xs, ys, weights = vec(cl.curmax), 
 		bins = cl.edges, alpha = 0.5, title = "curmax")
-	quiver!(vec(cl.x), vec(cl.y), quiver = quiver(cl.icurmax, mm, steps_))
+	quiver!(xs, ys, quiver = jc.cc_quiver(cl.icurmax, mm, steps_))
 end
 
 # ╔═╡ 62d11578-0d90-472d-8898-83e21c53d621
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.icurmax), 
+	histogram2d(xs, ys, weights = vec(cl.icurmax), 
 		bins = cl.edges, alpha = 0.5, title = "curmax direction")
 	#quiver!(vec(cl.x), vec(cl.y), quiver = quiver(cl.icurmax, mm, steps_))
 end
 
 # ╔═╡ 1605c6b4-f674-4209-ae46-f7ac4813693d
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.curmin), 
+	histogram2d(xs, ys, weights = vec(cl.curmin), 
 		bins = cl.edges, alpha = 0.5, title = "curmin direction")
-	quiver!(vec(cl.x), vec(cl.y), quiver = quiver(cl.icurmin, mm, steps_))
+	quiver!(xs, ys, quiver = quiver(cl.icurmin, mm, steps_))
 end
 
 # ╔═╡ f4155f75-af7f-4b79-b846-3bdc601d8767
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.icurmin), 
+	histogram2d(xs, ys, weights = vec(cl.icurmin), 
 		bins = cl.edges, alpha = 0.5, title = "curmin direction")
 	#quiver!(vec(cl.x), vec(cl.y), quiver = quiver(cl.icurmin, mm, steps_))
 end
 
 # ╔═╡ 654bce47-b51d-4c9a-81b3-b295f4bb055a
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.nodes), 
+	histogram2d(xs, ys, weights = vec(cl.nodes), 
 		bins = cl.edges, alpha = 1., title = "nodes")
 end
 
 # ╔═╡ 503b611f-d2f9-4b94-92c5-0b005505d5bf
 begin
-	histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.nborders), 
+	histogram2d(xs, ys, weights = vec(cl.nborders), 
 		bins = cl.edges, alpha = 1., title = "number of borders")
 end
 
 # ╔═╡ 27f27825-3f01-4c63-a119-4267ef69b11c
 begin
-nhis2 = [histogram2d(vec(cl.x), vec(cl.y), weights = vec(cl.neighbour_node[i]), 
+nhis2 = [histogram2d(xs, ys, weights = vec(cl.neighbour_node[i]), 
 		bins = cl.edges, title = m) for (i, m) in enumerate(mm.moves)]
 plot(nhis2..., layout = (3, 3))
 end
@@ -528,6 +286,7 @@ end
 # ╠═5dcb2929-115e-459c-b98d-43ae7bcabd3a
 # ╠═a9d9186f-19aa-41d7-8ec6-ad5197a74b8b
 # ╠═1751666d-d26f-4d8e-b5d6-b36854dd7462
+# ╠═a57cdb41-c388-4976-bec8-ec0650fb139c
 # ╠═cdc50171-b288-40b6-9d0d-9511901218e0
 # ╠═3922eba2-f322-4b06-b9e0-83bc723d7930
 # ╠═3aedeb39-f255-4fd5-9ac3-29888a129e90
@@ -535,18 +294,11 @@ end
 # ╠═94bb1cff-3f57-4af6-b177-ff732cc78429
 # ╠═7408b8f3-31f8-4764-bbf1-2bf9b245a8bb
 # ╠═40a8cad8-0a7b-41c5-9807-a5143c30a76b
-# ╠═e4e0ac6b-ad55-4a96-81e7-c5ec8df53be6
+# ╠═f5fb4efe-3d08-4d75-8855-670d0fe92f25
 # ╠═6faf5d14-f3f2-4549-bc1b-7d46771eb1da
-# ╠═b7fb1cb1-941f-4bfa-ad96-7addfd1e60c0
-# ╠═5cc3372f-f9df-4063-9e4c-dfa4e43f316c
 # ╠═8f49762d-9e98-4fed-b55c-17d9425d0fff
-# ╠═602dc3e2-bd48-4322-8687-42a03e1d0dd7
-# ╠═b06f35cd-368a-4eec-b21d-68f70afe59fa
 # ╠═62867032-c903-4b5b-b6db-51c02ddc6e6d
 # ╠═bc0a9ad4-62f4-4ef9-909a-4a2f68584b6f
-# ╠═6890e500-b267-4005-859f-405ac0ef895a
-# ╠═26bd9352-81d8-49de-a933-d62f22c462fa
-# ╠═cbf54f5a-1bdc-4473-b825-7d2f75211fc0
 # ╠═441a1b38-7ff8-456c-8511-98f57828b26b
 # ╠═7e1adeb7-2714-4205-b895-62cbe0477008
 # ╠═7856646f-20f1-47b1-986b-0702e2f53305
--- src/jclouds.jl
+++ src/jclouds.jl
@@ -1,5 +1,269 @@
 module jclouds
 
-# Write your package code here.
+using Base
+using StatsBase
+using LinearAlgebra
+
+export Img, Moves2D
+export cc_braid, cc_moves2d
+export cc_mesh, cc_quiver
+export cc_clouds
+
+#-------
+# Imgs
+#-------
+
+struct Img
+    coors
+    contents
+    edges
+end
+
+"""
+Create a braid Img (trenza) in 2D
+return imgs: (coors, contents, edges)
+"""
+function cc_braid(nn = 10)
+	nn = 10
+	zz = 0:nn-1
+	zz = vcat(zz, reverse(zz))
+	zz = ones(nn)' .* zz
+	xx = 0.5 .+ 0:2*nn
+	yy = 0.5 .+ 0:nn
+	xm = ones(nn)' .* xx
+	ym = yy'       .* ones(2*nn)
+	xbins = 0:1:2*nn
+	ybins = 0:1:nn
+    return Img((xm, ym), zz, (xbins, ybins))
+	#return (coors = (xm, ym), contents = zz, edges = (xbins, ybins))
+end
+
+"""
+convert a 2D histogram into img
+"""
+function histo_to_img(h::Histogram)
+	nx, ny  = Base.size(h.weights)
+	centers = [(edge[2:end] + edge[1:end-1])./2 for edge in h.edges]
+	xm = ones(ny)'   .* centers[1]
+	ym = centers[2]' .* ones(nx)
+	zz = deepcopy(h.weights)
+	return Img((xm, ym), zz, h.edges)
+end
+
+#-----------
+# Moves
+#-----------
+
+
+struct Moves2D
+	moves
+	imove0
+	dmoves
+	omoves
+end
+
+function _moves2d()
+	moves  = [[i, j] for i in -1:1:1 for j in -1:1:1]
+	imove0 = [i for (i, move) in enumerate(moves) if move == [0, 0]][1]
+	dmoves = Dict(1:9 .=> moves)
+	omoves = Dict()
+	imoves = deepcopy(moves)
+	imoves = filter(move -> move != imoves[imove0], moves)
+	for i in 1:9
+		omoves[moves[i]] = [imove for imove in imoves
+			if LinearAlgebra.dot(imove, moves[i]) == 0]
+	end
+	str_moves = Moves2D(moves, imove0, dmoves, omoves)
+end
+
+function cc_moves2d()
+    return _moves2d()
+end
+
+#-------
+#  Clouds
+#-------
+
+function _deltas(coors, energy, edges, steps, m)
+
+    xstep, ystep   = steps
+    xx, yy         = coors
+    xedges, yedges = edges
+
+    his      = [fit(Histogram, (xx .- mx * xstep, yy .- my * ystep), weights(energy), (xedges, yedges)) for (mx, my) in m.moves]
+    contents = deepcopy(his[m.imove0].weights)
+    deltas   = [h.weights .- contents for h in his]
+
+    dsteps   = [norm([xstep, ystep] .* move) for move in m.moves]
+    dsteps[m.imove0] = 1.
+    deltas  = [delta ./dstep for (delta, dstep) in zip(deltas, dsteps)]
+
+    return his, deltas
+end
+
+function _grad(deltas, m)
+    nx, ny = Base.size(deltas[m.imove0])
+    d0     = deltas[m.imove0]
+    grad   = deepcopy(d0)
+    igrad  = m.imove0 .* ones(Int, nx, ny)
+    for (i, di) in enumerate(deltas)
+        imask         = di .> grad
+        grad[imask]  .= di[imask]
+        igrad[imask] .= i
+    end
+    return grad, igrad
+end
+
+
+function _curvatures(deltas, m)
+
+    ddeltas = Dict()
+    for i in 1:9
+        ddeltas[m.moves[i]] = deltas[i]
+    end
+    curvs = Dict()
+    for imove in m.moves
+        curvs[imove] = reduce(.+, [ddeltas[move] for move in m.omoves[imove]])
+    end
+    return curvs
+end
+
+function _maxmin_curvatures(curves, m)
+    nx, ny = Base.size(curves[m.moves[m.imove0]])
+    curmin  =  1e6 .* ones(nx, ny)
+    icurmin = m.imove0 .* ones(Int, nx, ny)
+    curmax  = -1e6 .* ones(nx, ny)
+    icurmax = m.imove0 .* ones(Int, nx, ny)
+    for (i, move) in enumerate(m.moves)
+        dd = curves[move]
+        mask1 = dd .> curmax
+        curmax[mask1] .= dd[mask1]
+        icurmax[mask1] .= i
+        mask2 = dd .< curmin
+        curmin[mask2] .= dd[mask2]
+        icurmin[mask2] .= i
+    end
+    return curmax, icurmax, curmin, icurmin
+end
+
+#--- Nodes
+
+function _node(i, j, igrad, m)
+    index = CartesianIndex((i, j)...)
+    imove = igrad[index]
+    if (imove == m.imove0)
+        return index
+    else
+        index = [i, j] + m.moves[imove]
+        return _node(index[1], index[2], igrad, m)
+    end
+end
+
+function _nodes(contents, igrad, m)
+
+    cells = findall(x -> x.>0, contents)
+    nodes = [_node(cell[1], cell[2], igrad, m) for cell in cells]
+    unodes = unique(nodes)
+    inodes = Dict()
+    for (i, node) in enumerate(unodes)
+        inodes[node] = i
+    end
+
+    xnodes = Int.(0 .* deepcopy(contents))
+    for (k, cell) in enumerate(cells)
+        xnodes[cell] = inodes[nodes[k]]
+    end
+
+    return xnodes
+end
+
+function _neighbour_node(coors, nodes, edges, steps, m)
+
+    xstep, ystep   = steps
+    xx, yy         = coors
+    xedges, yedges = edges
+
+    his      = [fit(Histogram, (vec(xx) .- mx * xstep, vec(yy) .- my * ystep), weights(vec(nodes)), (xedges, yedges)) for (mx, my) in m.moves]
+    contents = deepcopy(his[m.imove0].weights)
+    mask     = contents .> 0
+    borders  = [(h.weights .>0) .* (h.weights .!= contents) for h in his]
+    nborders = reduce(.+, borders) .* mask
+
+    return nborders, [h.weights for h in his]
+end
+
+#--- Public
+
+function cc_mesh(edges)
+    nx, ny  = length(edges[1])-1, length(edges[2])-1
+    centers = [(edge[2:end] + edge[1:end-1])./2 for edge in edges]
+    xm = ones(ny)'   .* centers[1]
+    ym = centers[2]' .* ones(nx)
+    return xm, ym
+end
+
+
+function cc_quiver(idir, m, steps)
+    xstep, ystep = steps
+    uus = [m.dmoves[k] for k in vec(idir)]
+    us = [xi * 0.8 * xstep for (xi, yi) in uus]
+    vs = [yi * 0.8 * ystep for (xi, yi) in uus]
+    return us, vs
+end
+
+function cc_clouds(coors, energy, steps)
+
+    ndim  = length(coors)
+    nsize = length(coors[1])
+
+    # assert dimensions
+    for i in 2:ndim
+        @assert(length(coors[2]) == nsize)
+    end
+    @assert(length(energy) == nsize)
+    @assert(length(steps)  == ndim)
+
+    # define the extended edges
+    edges = [minimum(x)-1.5*step:step:maximum(x)+1.5*step for (x, step) in zip(coors, steps)]
+
+    # alias
+    xx, yy         = coors
+    xedges, yedges = edges
+
+    # main histogram
+    histo    = fit(Histogram, (xx, yy), weights(energy), (xedges, yedges))
+    contents = deepcopy(histo.weights)
+
+    # deltas
+    m = _moves2d()
+    his, deltas = _deltas(coors, energy, edges, steps, m)
+
+    # gradient
+    grad, igrad = _grad(deltas, m)
+
+    # curvatures
+    curves = _curvatures(deltas, m)
+    lap    = curves[m.moves[m.imove0]]
+
+    # maximum and monimum curvatures
+    curmax, icurmax, curmin, icurmin = _maxmin_curvatures(curves, m)
+
+    xs, ys = cc_mesh(edges)
+
+    # nodes
+    xnodes = _nodes(contents, igrad, m)
+    xborders, xneigh = _neighbour_node((xs, ys), xnodes, edges, steps, m)
+
+    # output
+    return (edges = (xedges, yedges),  coors = (xs, ys), contents = contents,
+            grad = grad, igrad = igrad,
+            lap = lap, curves = curves,
+            curmax = curmax, icurmax = icurmax,
+            curmin = curmin, icurmin = icurmin,
+            nodes  = xnodes,
+            nborders = xborders, neighbour_node = xneigh)
 
 end
+
+
+end # end of module
